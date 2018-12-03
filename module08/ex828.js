ea0598e06c14cb95e2270f3fd79de19200b7ef8b
sabawoon sarwari <sabawoonsarwari@gmail.com>
2018-12-03 20:22:54 +0200

    Motion

--- module08/ex828.js
+++ module08/ex828.js
@@ -1,30 +1,23 @@
-// create a variable to control the direction of the ellipse
-let control = false;
-// make ellipse starting from the left end of the canvas
-let i=30;
+let	x= 300;
+let	y= 200;
+let	speedx= 4;
+let speedy= -3;
+let r = 24;
 function setup() {
-  // create canvas
-  createCanvas(800,500);
-  // set background color
-  background('#31bc33');
-}
-function draw () {
-  // when ellipse is in the left edge, control would be/become false
-  if (i==30) {
-    control = false;
-  }
-  // if ellipse is inside the canvas width (we substract 30 which is the radius
-  // of our ellipse) and control is false
-  if (i<(width-30) && control==false){
-    i++
-    // increase its x position (ellipse moves in the right direction)
-    ellipse(i,height/2,60);
-  }
-  else {
-    // else control will change to true
-    control = true;
-    // decrease its x position (ellipse moves in the left direction)
-    i--;
-    ellipse(i,height/2,60);
-  }
+	createCanvas(600,400);
+}	
+function draw() {
+	background(0);
+	strokeWeight(4);
+	stroke(255);
+	fill(255, 0, 0);
+	x = x + speedx;
+	y = y + speedy;
+	if (x > (width-r/2) || x- r/2< 0) {
+		speedx = speedx* (-1);
+	}
+	if (y> (height-r/2)  || y -r/2 < 0) {
+		speedy = speedy* (-1);
+	}
+	ellipse(x, y ,r);	
 }
